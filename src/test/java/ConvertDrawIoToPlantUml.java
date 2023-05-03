a220fcf0e7ba989363246d83f080d6bd75fb3986
igor.suhorukov <igor.suhorukov@gmail.com>
2023-05-04 00:16:56 +0300

    Support composite state in conversion

--- src/test/java/ConvertDrawIoToPlantUml.java
+++ src/test/java/ConvertDrawIoToPlantUml.java
@@ -23,48 +23,68 @@ public class ConvertDrawIoToPlantUml {
             this.rule = rule;
         }
     }
+    private static class State{
+        String state;
+        String description;
+
+        public State(String state, String description) {
+            this.state = state;
+            this.description = description;
+        }
+    }
     public static void main(String[] args) throws Exception{
         String file = ConvertDrawIoToPlantUml.class.getResource("Untitled Diagram.drawio").getFile();
         Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
         XPath xPath = XPathFactory.newInstance().newXPath();
-        Map<String,String> states = new LinkedHashMap<>();
+        Map<String,State> states = new LinkedHashMap<>();
         List<Transition> transitions = new ArrayList<>();
         NodeList expressions = (NodeList) xPath.evaluate(
-                            "//mxCell[starts-with(./@style,'edgeLabel')]", document, XPathConstants.NODESET);
+                            "//mxCell[starts-with(./@style,'edgeLabel') " +
+                                    " or (starts-with(./@style,'edgeStyle') and @value)]",
+                document, XPathConstants.NODESET);
         for(int idx = 0; idx < expressions.getLength(); idx++){
             Element item = (Element) expressions.item(idx);
             String edgeRule = item.getAttribute("value");
             String parent = item.getAttribute("parent");
-            Element edge = getElement(document, xPath, parent);
+            Element edge = "1".equals(parent)? item : getElement(document, xPath, parent);
             String source = edge.getAttribute("source");
             String target = edge.getAttribute("target");
             Element sourceState = getElement(document, xPath, source);
             String sourceStateName = getStateName(sourceState);
-            states.put(source, sourceStateName);
+            String sourceDescription = getDescription(document, xPath, sourceState);
+            states.put(source, new State(sourceStateName, sourceDescription));
             Element targetState = getElement(document, xPath, target);
             String targetStateName = getStateName(targetState);
-            states.put(target, targetStateName);
+            String targetDescription = getDescription(document, xPath, targetState);
+            states.put(target, new State(targetStateName, targetDescription));
             transitions.add(new Transition(source,target,edgeRule));
         }
         Map<String, String> stateIdToNumber = new HashMap<>();
-        String initState = states.entrySet().stream().filter(stringStringEntry -> "startState".equals(stringStringEntry.getValue())).findFirst().get().getKey();
+        String initState = states.entrySet().stream().filter(stringStringEntry ->
+                "startState".equals(stringStringEntry.getValue().state)).findFirst().get().getKey();
         stateIdToNumber.put(initState, "[*]");
-        String endState = states.entrySet().stream().filter(stringStringEntry -> "endState".equals(stringStringEntry.getValue())).findFirst().get().getKey();
+        String endState = states.entrySet().stream().filter(stringStringEntry ->
+                "endState".equals(stringStringEntry.getValue().state)).findFirst().get().getKey();
         stateIdToNumber.put(endState, "[*]");
-        List<Map.Entry<String, String>> entries = states.entrySet().stream().
-                filter(stringStringEntry -> !("startState".equals(stringStringEntry.getValue())
-                                                || "endState".equals(stringStringEntry.getValue()))).
+        List<Map.Entry<String, State>> entries = states.entrySet().stream().
+                filter(stringStringEntry -> !("startState".equals(stringStringEntry.getValue().state)
+                                                || "endState".equals(stringStringEntry.getValue().state))).
                 collect(Collectors.toList());
         for(int idx = 0; idx < entries.size(); idx++){
             stateIdToNumber.put(entries.get(idx).getKey(), Integer.toString(idx));
         }
         StringBuilder plantUml = new StringBuilder();
-        plantUml.append("@startuml\n");
-        states.forEach((s, s2) -> {
-            if("[*]".equals(stateIdToNumber.get(s))){
+        plantUml.append("@startuml\nhide empty description\n");
+        states.forEach((stateId, state) -> {
+            if("[*]".equals(stateIdToNumber.get(stateId))){
                 return;
             }
-            plantUml.append("state \"").append(s2).append("\" as state").append(stateIdToNumber.get(s)).append("\n");
+            plantUml.append("state \"").append(state.state).append("\" as state").
+                    append(stateIdToNumber.get(stateId)).append("\n");
+            if(state.description != null){
+                plantUml.append("state").append(stateIdToNumber.get(stateId)).append(" : ").
+                        append(state.description).append("\n");
+            }
         });
         transitions.forEach(transition -> {
             plantUml.append("[*]".equals(stateIdToNumber.get(transition.from))?"":"state").
@@ -75,6 +95,14 @@ public class ConvertDrawIoToPlantUml {
         });
         plantUml.append("@enduml\n");
         System.out.println(plantUml.toString());
+    }
+
+    private static String getDescription(Document document, XPath xPath, Element state) throws XPathExpressionException {
+        String id = state.getAttribute("id");
+        String description = (String) xPath.evaluate(
+                String.format("//mxCell[./@parent='%s' and starts-with(./@style,'text')]/@value", id),
+                document, XPathConstants.STRING);
+        return !description.isEmpty() ? description: null;
     }
 
     private static String getStateName(Element state) {
